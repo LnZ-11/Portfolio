d385ea18c7464f53aa691a0c20436395d267e7f3
LnZ-11 <zlyes1999@gmail.com>
2025-07-19 14:55:55 +0100

    fixe Navbar behaviour

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -6,19 +6,24 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 
-export default function Navbar(){
-  const [isOpen, setIsOpen] = useState(false);
+interface NavbarProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  onClicking: () => void;
+  }
+
+export default function Navbar({ isOpen, setIsOpen, onClicking }: NavbarProps) {
 
 return(
-  <nav className="sticky top-0 z-50 rounded-lg shadow-lg overflow-hidden p-2 bg-[#C4C4C4FF] shadow-stone-950/5 mx-auto w-full max-w-screen-xl">
+  <nav className="sticky top-0 z-50 md:rounded-b-lg shadow-lg overflow-hidden p-2 bg-[#C4C4C4FF] shadow-stone-950/5 mx-auto w-full max-w-screen-xl">
   <div className="flex items-center">
     <RiAccountCircleLine className='text-black'/>
-  <Link href="/" className="font-sans antialiased text-sm text-black ml-2 mr-2 block py-1 font-semibold">Lyes Lattari</Link>
-  <hr className="mx-1 hidden h-5 w-px border-l border-t-0 border-secondary-dark lg:block" />
+  <Link href="/" onClick={onClicking} className="font-sans antialiased text-sm text-black ml-2 mr-2 block py-1 font-semibold">Lyes Lattari</Link>
+  <hr onClick={onClicking} className="mx-1 hidden h-5 w-px border-l border-t-0 border-secondary-dark lg:block" />
   <div className="hidden lg:block">
-  <div className="min-w-60 mt-4 flex flex-col gap-1 md:text-center md:mt-0 md:flex-row md:items-center">
+  <div onClick={onClicking} className="min-w-60 mt-4 flex flex-col gap-1 md:text-center md:mt-0 md:flex-row md:items-center">
   <div className="flex flex-row justify-center text-center" data-dui-placement="bottom-start">
-  </div>
+  </div >
         <Link href="/projects" className="flex items-center py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in aria-disabled:opacity-50 aria-disabled:pointer-events-none bg-transparent text-stone-600 hover:text-stone dark:hover:text-black hover:bg-stone-200 focus:bg-stone-200 focus:text-stone-800 dark:focus:text-black dark:data-[selected=true]:text-white dark:bg-opacity-70">
           <span className="grid place-items-center shrink-0 me-2.5 mr-1.5"><FaLaptopCode/></span>
           <small className="font-sans antialiased text-sm text-current">Projects</small>
@@ -44,7 +49,7 @@ return(
     </button>
     <Image src="/Images/profilePic.jpg" alt="profile-picture" width={32} height={32} className="inline-block object-cover object-center w-8 h-8 rounded outline-none group border border-stone-800 p-0.5 lg:ml-auto" />
   </div>
-  <div className={`overflow-scroll transition-all duration-300 ease-in-out ${isOpen ? 'max-h-96' : 'max-h-0'}`} id="navbar-collapse">
+  <div onClick={onClicking} className={`overflow-scroll transition-all duration-300 ease-in-out ${isOpen ? 'max-h-96' : 'max-h-0'}`} id="navbar-collapse">
     <div className="min-w-60 mt-4 flex flex-col gap-1">
       <Link href="/projects" className="flex items-center py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in aria-disabled:opacity-50 aria-disabled:pointer-events-none bg-transparent text-stone-600 hover:text-stone-800 dark:hover:text-white hover:bg-stone-200 focus:bg-stone-200 focus:text-stone-800 dark:focus:text-white dark:data-[selected=true]:text-white dark:bg-opacity-70">
         <span className="grid place-items-center shrink-0 me-2.5"><FaLaptopCode/></span>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,33 @@
-
 "use client"
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import "./globals.css";
 import WaveBackground from "./components/waveBackground";
+import { useState } from 'react';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isOpen, setIsOpen] = useState(false);
+  const clicked = () => {
+    setIsOpen(true? false: true);
+  }
+
   return (
     <html lang="en">
       <body 
       className={"h-auto w-full"}>
-        <main className="flex flex-col w-full max-w-5xl gap-4 justify-center text-center text-black"/>
-        <WaveBackground>
-          <div className="relative min-h-screen z-1">
-
-        <Navbar/>
-        {children}
+        <WaveBackground >
+            <Navbar isOpen={isOpen} setIsOpen={setIsOpen} onClicking={clicked}/>
+          <main 
+        className="flex flex-col w-full max-w-5xl gap-4 justify-center text-center text-black"/>
+          <div className="relative min-h-screen z-1" onClick={clicked}>
+            {children}
           </div>
-        </WaveBackground>
         <Footer/>
+        </WaveBackground>
       </body>
     </html>
   );
